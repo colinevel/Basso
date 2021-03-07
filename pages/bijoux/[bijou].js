699387da256bd25d9f050e928dae08d442055f49
colinevel <coline.velard@gmail.com>
2021-03-07 14:29:08 +0100

    Standardize images

--- pages/bijoux/[bijou].js
+++ pages/bijoux/[bijou].js
@@ -3,11 +3,32 @@ import styles from "../../styles/Bijou.module.css";
 import { fetchAPI } from "../../api/api.js";
 
 function Bijou({ bijou }) {
+  const description = bijou.description;
+  const imagesArray = bijou.images;
+  console.log("rrrr", imagesArray);
 
   return (
     <div className={styles.container}>
-      <TopBar />    
+      <TopBar />
       <h2>{bijou.title}</h2>
+      <div className={styles.imagesList}>
+        {imagesArray.map((image, index) => {
+          return (
+            <li key={index}>
+              <div className={styles.imageContainer}>
+                <img
+                  alt={`${image.alternativeText}`}
+                  src={`http://localhost:1337${image.url}`}
+                />
+              </div>
+            </li>
+          );
+        })}
+      </div>
+      <div
+        className={styles.description}
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
     </div>
   );
 }
@@ -16,7 +37,7 @@ export async function getStaticPaths() {
   const bijoux = await fetchAPI("/bijouxes");
 
   const paths = bijoux.map((bijou) => ({
-    params: { bijou: bijou.id.toString() }
+    params: { bijou: bijou.id.toString() },
   }));
 
   return { paths, fallback: false };
